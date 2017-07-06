f35f893e228d56cd2a08217b837dcf25f464b0cf
kaeross <kaeross@icloud.com>
2017-07-06 16:54:01 +0100

    Update

--- js/app.js
+++ js/app.js
@@ -1,8 +1,14 @@
 (function ticTacToe() {
+
 	const body = document.body;
 	const board = document.getElementById("board");
 	const startScreen = document.getElementById("start");
 	const endScreen = document.getElementById("finish");
+	const player1 = document.querySelector("#player1");
+	const player2 = document.querySelector("#player2");
+	const playerOne = new Player(player1, "url('img/o.svg')", "player1");
+	const playerTwo = new Player(player2, "url('img/x.svg')", "player2");
+
 
 	function showPage(show, hide1, hide2) {
 		show.style.display = 'block';
@@ -23,36 +29,72 @@
 			const playersNames = `<div class="playerNames"><h2 class="playerName player1name">${namePrompt}</h2><h2 class="playerName player2name">Computer</h2></div>`;
 			boardHeader.insertAdjacentHTML('beforeend', playersNames);
 		})();
+		alert("Please select a player to start");
 
 	});
 
+	function activate(player) {
+		if (player === player1) {
+			player1.className = "active players player1";
+			player2.className = "players player2";
+		}
+		else if (player === player2) {
+			player2.className = "active players player2";
+			player1.className = "players player1";
+		}
+	}
+
+	//Choose player to start
+	(function selectPlayer() {
+		player1.addEventListener('click', function(){
+			activate(player1)
+		});
+		player2.addEventListener('click', function(){
+			activate(player2)
+		});
+	})();
+
+	function isActive(player) {
+		if (player.classList.contains('active')) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	function boxhover(event, bgImage1, bgImage2) {
+		const allBox = document.getElementsByClassName("box");
+		for (let i = 0; i < allBox.length; i += 1) {
+			allBox[i].addEventListener( event, function(e) {
+				var box = e.target;
+				if (isActive(player1)) {
+					box.style.backgroundImage = bgImage1;
+				} else if (isActive(player2)) {
+					box.style.backgroundImage = bgImage2;
+				}
+			});
+		}
+	}
+
+	boxhover('mouseenter', 'url("img/o.svg")', 'url("img/x.svg")');
+	boxhover('mouseleave', '', '');
+
+	function boxClick() {
+		boxhover('click', 'url("img/o.svg")', 'url("img/x.svg")');
+		if (isActive(player1)) {
+			activate(player2);
+		} else if (isActive(player2)) {
+			activate(player1);
+		}
+	}
 
 
-	// var player1 = document.querySelector("#player1");
-	// var player2 = document.querySelector("#player2");
-	// player1.className = "active players player1";
-	// function activate(player) {
-		
 
-		
 
-	// 	else if (player2) {
-	// 		player2.className = "active players player2";
-	// 	}
-	// }
 
-	// activate(player1);
+
+
+
 
 })();
 
-// var playerOne = document.querySelector(".player1");
-// var player1 = new Player(playerOne);
-//Indicate current player
-//if active add id "player1"
-	//add class "active"
-
-//if player 1 mouse over board
-	//background-image: url("img/o.svg");
-
-//if player 2 mouse over board
-	//background-image: url("img/x.svg");
