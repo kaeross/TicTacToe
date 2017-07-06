1498fb1c87e29dd2bbb462a96e65fa9519da1702
kaeross <kaeross@icloud.com>
2017-07-06 16:54:16 +0100

    Created player constructor

--- js/player.js
+++ js/player.js
@@ -1,16 +1,25 @@
-function Player(player, svg) {
+function Player(player, svg, id) {
 	this.player = player;
 	this.svg = svg;
+	this.id = id;
 }
 
-Player.prototype.isActive = function(id){
-	if (this.className = `active players ${this.id}`) {
+Player.prototype.isActive = function(){
+	if (this.player.classList.contains('active')) {
 		return true;
 	} else {
 		return false;
 	}
 };
 
-Player.prototype.makeActive = function(activatePlayer, deactivatePlayer){
-	
-};
+Player.prototype.activate = function(){
+	if (this.player === player1) {
+		this.player.className = `active players ${this.id}`;
+		player2.className = "players player2";
+	}
+	else if (this.player === player2) {
+		this.player.className = `active players ${this.id}`;
+		player1.className = "players player1";
+	}
+}
+
